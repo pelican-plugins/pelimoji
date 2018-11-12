6e59750ef76291a3ab90d9807f1a3acc1947b7ae
Kay Ohtie <kay@coyotesin.space>
2018-11-11 22:49:12 -0600

    Changed to using spritemap. Now requires glue

--- pelimoji.py
+++ pelimoji.py
@@ -1,27 +1,40 @@
-import pelican, sys, glob, os.path
+import pelican, sys, subprocess, os.path, glob
 
 def init(pelican_object):
-    global emojis, emojipath
+    global emojis, prefix
     # Let's build a list of installed emoji
-    contentroot = pelican_object.settings.get('PATH',())
-    emojipath = pelican_object.settings.get('PELIMOJI_PATH', ())
-    searchpath = "%s/%s/*.png" % (contentroot,emojipath)
-    installed_emoji = glob.glob(searchpath)
+    content_root = pelican_object.settings.get('PATH',())
+    search_path = "%s/emoji" % (content_root,)
+    output_path = "emoji_map"
+    save_path = "%s/%s" % (content_root,output_path)
+    pelican_object.settings['STATIC_PATHS'].append(output_path)
+    prefix = pelican_object.settings.get('PELIMOJI_PREFIX',"")
+    if prefix != "":
+        prefix = prefix + "-"
+    installed_emoji = subprocess.check_output(['find',search_path,'-iname',"*.png"]).split(b'\n')
+    glue_exec = ['glue',search_path, save_path, \
+        '--namespace',"cemoji", \
+        '--sprite-namespace',"", \
+        '--css-template=plugins/pelimoji/css.j2', \
+        '--recursive', ]\
+        #'--padding=0.5',] \
+        #'--png8']
+    subprocess.call(glue_exec)
     # Great! Now let's create a search-list
-    emojis = [ os.path.basename(x)[:-4] for x in installed_emoji ]
+    emojis = [ os.path.basename(x.decode('utf-8'))[:-4] for x in installed_emoji ]
 
 def replace(path, context):
     # Now, we'll open any written files to search for the matching search-strings
     with open(path, 'r') as f:
         s = f.read()
         for emoji in emojis:
-            search = ':%s:' % (emoji,)
+            search = ':%s%s:' % (prefix,emoji)
             s = s.replace(search, emoji_html(emoji))
     with open(path, 'w') as f:
         f.write(s)
 
 def emoji_html(emoji):
-    html = '<img class="pelimoji" src="/%s/%s.png" class="pelimoji" title=":%s:" alt=":%s:" />' % (emojipath,emoji,emoji,emoji)
+    html = '<i class="cemoji cemoji-%s" title=":%s:"><span>:%s:</span></i>' % (emoji,emoji,emoji)
     return html
 
 def register():

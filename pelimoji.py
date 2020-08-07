f80a4c129c4e70b942d097b95406e8239066b992
Kay Ohtie <kay@coyotesin.space>
2020-08-06 20:11:25 -0500

    revised search method to use regex instead of iterating every emoji across every file

--- pelimoji.py
+++ pelimoji.py
@@ -1,7 +1,8 @@
-import pelican, sys, subprocess, os.path, glob
+import pelican, sys, subprocess, os.path, glob, re
 
 def init(pelican_object):
-    global emojis, prefix
+    #global emojis, prefix
+    global pelimoji_prog, pelimoji_replace
     # Let's build a list of installed emoji
     content_root = pelican_object.settings.get('PATH',())
     search_path = "%s/emoji" % (content_root,)
@@ -12,6 +13,7 @@ def init(pelican_object):
     if prefix != "":
         prefix = prefix + "-"
     installed_emoji = subprocess.check_output(['find',search_path,'-iname',"*.png"]).split(b'\n')
+    # Now using 'glue' to make the sprite sheet itself
     glue_exec = ['glue',search_path, save_path, \
         '--namespace',"cemoji", \
         '--sprite-namespace',"", \
@@ -20,20 +22,22 @@ def init(pelican_object):
         #'--padding=0.5',] \
         #'--png8']
     subprocess.call(glue_exec)
-    # Great! Now let's create a search-list
+    # Now let's create a search-list of emoji names
     emojis = [ os.path.basename(x.decode('utf-8'))[:-4] for x in installed_emoji ]
-
-def emoji_html(emoji):
-    html = '<i class="cemoji cemoji-%s" title=":%s:"><span>:%s:</span></i>' % (emoji,emoji,emoji)
-    return html
+    # And a regex pattern to find to avoid iterating
+    pattern = ":" + prefix + "(?P<emoji>(" + ")|(".join(emojis) + ")):"
+    # compile the pattern to global for speed given how massive it'll (potentially) be
+    pelimoji_prog = re.compile(pattern)
+    # And the pattern to replace it with!
+    pelimoji_replace = "<i class=\"cemoji cemoji-\g<emoji>\" title=\":\g<emoji>:\"><span>:\g<emoji>:</span></i>"
 
 def replace(content):
-    try:
-        for emoji in emojis:
-            search = ':%s%s:' % (prefix,emoji)
-            content._content = content._content.replace(search, emoji_html(emoji))
-    except:
-        print("Could not edit content of %s" % (content,))
+    fileext = str(content).split('.')[-1].lower()
+    if fileext in ['md','html','rst','txt']:
+        try:
+            content._content = pelimoji_prog.sub(pelimoji_replace,content._content)
+        except:
+            print("Something went wrong editing {} for pelimoji, sorry".format(str(content)))
 
 def register():
     pelican.signals.initialized.connect(init)

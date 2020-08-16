73ce0db5e78a184b4545f173d4e18275d0764ccf
Justin Mayer <entroP@gmail.com>
2020-08-16 20:08:37 +0200

    Adjust path to pelimoji/css.j2 file

--- pelican/plugins/pelimoji/pelimoji.py
+++ pelican/plugins/pelimoji/pelimoji.py
@@ -1,7 +1,15 @@
-import pelican, sys, subprocess, os.path, glob, re
+from os.path import abspath, basename, dirname
+from pathlib import Path
+import re
+import subprocess
+
+import pelican
+
+PLUGIN_ROOT = Path(dirname(dirname(abspath(__file__))))
+
 
 def init(pelican_object):
-    #global emojis, prefix
+    # Global emojis, prefix
     global pelimoji_prog, pelimoji_replace
     # Let's build a list of installed emoji
     content_root = pelican_object.settings.get('PATH',())
@@ -17,18 +25,19 @@ def init(pelican_object):
     glue_exec = ['glue',search_path, save_path, \
         '--namespace',"cemoji", \
         '--sprite-namespace',"", \
-        '--css-template=plugins/pelimoji/css.j2', \
-        '--recursive', 
+        f'--css-template={PLUGIN_ROOT}/pelimoji/css.j2', \
+        '--recursive',
         '--cachebuster' ]
     subprocess.call(glue_exec)
     # Now let's create a search-list of emoji names
-    emojis = [ os.path.basename(x.decode('utf-8'))[:-4] for x in installed_emoji ]
+    emojis = [ basename(x.decode('utf-8'))[:-4] for x in installed_emoji ]
     # And a regex pattern to find to avoid iterating
     pattern = ":" + prefix + "(?P<emoji>(" + ")|(".join(emojis) + ")):"
-    # compile the pattern to global for speed given how massive it'll (potentially) be
+    # compile pattern to global for speed, given how massive it'll (potentially) be
     pelimoji_prog = re.compile(pattern)
     # And the pattern to replace it with!
     pelimoji_replace = "<i class=\"cemoji cemoji-\g<emoji>\" title=\":\g<emoji>:\"><span>:\g<emoji>:</span></i>"
+
 
 def replace(content):
     fileext = str(content).split('.')[-1].lower()
@@ -37,6 +46,7 @@ def replace(content):
             content._content = pelimoji_prog.sub(pelimoji_replace,content._content)
         except:
             print("Something went wrong editing {} for pelimoji, sorry".format(str(content)))
+
 
 def register():
     pelican.signals.initialized.connect(init)
